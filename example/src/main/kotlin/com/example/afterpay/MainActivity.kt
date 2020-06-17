c8a96adebfd2f975c40f88bbb69228f82d938faa
Ryan Davis <ryan@ittybittyapps.com>
2020-06-17 11:33:22 +1000

    Enable sensor based orientation changes in example

--- example/src/main/kotlin/com/example/afterpay/MainActivity.kt
+++ example/src/main/kotlin/com/example/afterpay/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.afterpay
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
 
         val emailField = findViewById<EditText>(R.id.main_emailAddress)
         emailField.addTextChangedListener { text ->
