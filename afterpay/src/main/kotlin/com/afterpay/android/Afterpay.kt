716c18bd019f359c1cf81177d7bc111065bbe873
Ryan Davis <ryan@ittybittyapps.com>
2020-06-18 08:06:10 +1000

    Improve consistency of SDK API documentation

--- afterpay/src/main/kotlin/com/afterpay/android/Afterpay.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/Afterpay.kt
@@ -9,8 +9,8 @@ import com.afterpay.android.view.WebCheckoutActivity
 object Afterpay {
     /**
      * Creates an [Intent] that can be used to initiate an Afterpay transaction. Provide the
-     * new [Intent] to [startActivityForResult][android.app.Activity.startActivityForResult]
-     * to initiate the transaction.
+     * new [Intent] in [startActivityForResult][android.app.Activity.startActivityForResult]
+     * to initiate the checkout process.
      *
      * @param context The calling activity context.
      * @param checkoutUrl The URL used to initiate the transaction.
@@ -21,10 +21,10 @@ object Afterpay {
             .putCheckoutUrlExtra(checkoutUrl)
 
     /**
-     * Parses the status from the checkout transaction.
+     * Parses the status of the Afterpay checkout transaction.
      *
-     * @param intent The intent returned from
-     * [startActivityForResult][android.app.Activity.startActivityForResult]
+     * @param intent The intent returned in
+     * [startActivityForResult][android.app.Activity.startActivityForResult].
      * @return The checkout status.
      */
     fun parseCheckoutResponse(intent: Intent): CheckoutStatus =
