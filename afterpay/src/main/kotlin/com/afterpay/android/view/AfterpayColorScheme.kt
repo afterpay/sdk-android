c2d5e1b512425bc980ca83313b29b15930d225e6
Ryan Davis <ryan@ittybittyapps.com>
2020-08-04 09:32:59 +1000

    Add new Afterpay badge assets

--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayColorScheme.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayColorScheme.kt
@@ -3,16 +3,20 @@ package com.afterpay.android.view
 import com.afterpay.android.R
 
 enum class AfterpayColorScheme {
+    BLACK_ON_MINT,
+    MINT_ON_BLACK,
     WHITE_ON_BLACK,
     BLACK_ON_WHITE;
 
     internal companion object {
-        val DEFAULT: AfterpayColorScheme get() = WHITE_ON_BLACK
+        val DEFAULT: AfterpayColorScheme get() = BLACK_ON_MINT
     }
 }
 
 internal val AfterpayColorScheme.badgeDrawable: Int
     get() = when (this) {
+        AfterpayColorScheme.BLACK_ON_MINT -> R.drawable.afterpay_badge_black_on_mint
+        AfterpayColorScheme.MINT_ON_BLACK -> R.drawable.afterpay_badge_mint_on_black
         AfterpayColorScheme.WHITE_ON_BLACK -> R.drawable.afterpay_badge_white_on_black
         AfterpayColorScheme.BLACK_ON_WHITE -> R.drawable.afterpay_badge_black_on_white
     }
