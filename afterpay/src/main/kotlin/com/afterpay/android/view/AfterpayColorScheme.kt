ef997ca43c6cd1d3f00c6355440290dadcf753ae
Ryan Davis <ryan@ittybittyapps.com>
2020-08-04 10:08:21 +1000

    Add pay now with Afterpay button

--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayColorScheme.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayColorScheme.kt
@@ -20,3 +20,11 @@ internal val AfterpayColorScheme.badgeDrawable: Int
         AfterpayColorScheme.WHITE_ON_BLACK -> R.drawable.afterpay_badge_white_on_black
         AfterpayColorScheme.BLACK_ON_WHITE -> R.drawable.afterpay_badge_black_on_white
     }
+
+internal val AfterpayColorScheme.payNowButtonDrawable: Int
+    get() = when (this) {
+        AfterpayColorScheme.BLACK_ON_MINT -> R.drawable.afterpay_button_pay_now_black_on_mint
+        AfterpayColorScheme.MINT_ON_BLACK -> R.drawable.afterpay_button_pay_now_mint_on_black
+        AfterpayColorScheme.WHITE_ON_BLACK -> R.drawable.afterpay_button_pay_now_white_on_black
+        AfterpayColorScheme.BLACK_ON_WHITE -> R.drawable.afterpay_button_pay_now_black_on_mint
+    }
