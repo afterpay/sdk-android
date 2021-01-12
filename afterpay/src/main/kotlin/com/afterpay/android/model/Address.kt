69997502942f6e12159f5bf836c0dcd1ab16f6b4
Adam Campbell <adam.campbell@ittybittyapps.com>
2021-01-13 09:57:19 +1100

    Add Address model

---
+++ afterpay/src/main/kotlin/com/afterpay/android/model/Address.kt
@@ -0,0 +1,9 @@
+package com.afterpay.android.model
+
+data class Address(
+    val countryCode: String?,
+    val postcode: String?,
+    val phoneNumber: String?,
+    val state: String?,
+    val suburb: String?
+)
