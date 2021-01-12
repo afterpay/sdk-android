b163870e4d7abbdc1b6f9cc662d1ee5fd18432f9
Adam Campbell <adam.campbell@ittybittyapps.com>
2021-01-12 17:10:28 +1100

    Add ShippingOption and Money data classes

---
+++ afterpay/src/main/kotlin/com/afterpay/android/model/ShippingOption.kt
@@ -0,0 +1,15 @@
+package com.afterpay.android.model
+
+data class ShippingOption(
+    val id: String,
+    val name: String,
+    val description: String,
+    var shippingAmount: Money,
+    var orderAmount: Money,
+    var taxAmount: Money?
+)
+
+data class Money(
+    val amount: String,
+    val currency: String
+)
