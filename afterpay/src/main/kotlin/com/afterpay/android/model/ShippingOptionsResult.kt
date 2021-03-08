dd4272d35d1f3aad76521083aaa6e2f75c3c38a0
Adam Campbell <adam.campbell@ittybittyapps.com>
2021-03-09 08:39:46 +1100

    Remove unused string value from ShippingOptionsError

--- afterpay/src/main/kotlin/com/afterpay/android/model/ShippingOptionsResult.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/model/ShippingOptionsResult.kt
@@ -10,9 +10,9 @@ data class ShippingOptionsErrorResult(
     val error: ShippingOptionsError
 ) : ShippingOptionsResult()
 
-enum class ShippingOptionsError(val string: String) {
-    SHIPPING_ADDRESS_UNRECOGNIZED("SHIPPING_ADDRESS_UNRECOGNIZED"),
-    SHIPPING_ADDRESS_UNSUPPORTED("SHIPPING_ADDRESS_UNSUPPORTED"),
-    SERVICE_UNAVAILABLE("SERVICE_UNAVAILABLE"),
-    BAD_RESPONSE("BAD_RESPONSE")
+enum class ShippingOptionsError {
+    SHIPPING_ADDRESS_UNRECOGNIZED,
+    SHIPPING_ADDRESS_UNSUPPORTED,
+    SERVICE_UNAVAILABLE,
+    BAD_RESPONSE
 }
