284e6f740e6dbc3c85d8289de2c06ac819dbde3e
Kieran O'Brien <kieran.obrien@ittybittyapps.com>
2021-04-27 16:56:01 +1000

    Add total amount field to receipt screen

--- example/src/main/kotlin/com/example/afterpay/receipt/ReceiptFragment.kt
+++ example/src/main/kotlin/com/example/afterpay/receipt/ReceiptFragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.activity.addCallback
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.afterpay.android.view.AfterpayWidgetView
 import com.example.afterpay.R
 import com.example.afterpay.nav_graph
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.onEach
 
 class ReceiptFragment : Fragment() {
 
@@ -32,7 +42,25 @@ class ReceiptFragment : Fragment() {
             findNavController().navigate(nav_graph.action.back_to_shopping)
         }
 
-        view.findViewById<AfterpayWidgetView>(R.id.widget)
-            .apply { init(viewModel.token) }
+        view.findViewById<AfterpayWidgetView>(R.id.receipt_widget)
+            .apply {
+                init(viewModel.token)
+                viewModel
+                    .totalCost()
+                    .onEach { update(it) }
+                    .launchIn(viewLifecycleOwner.lifecycleScope)
+            }
+
+        view.findViewById<EditText>(R.id.receipt_totalCost).apply {
+            callbackFlow {
+                doOnTextChanged { text, _, _, _ -> trySend(text.toString()) }
+                    .also { awaitClose { removeTextChangedListener(it) } }
+            }
+                .mapNotNull { it.toBigDecimalOrNull() }
+                .conflate()
+                .debounce(timeoutMillis = 300)
+                .onEach { viewModel.onTotalCost(it) }
+                .launchIn(viewLifecycleOwner.lifecycleScope)
+        }
     }
 }
