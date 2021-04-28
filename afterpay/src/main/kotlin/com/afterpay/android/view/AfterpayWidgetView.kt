fafa43a77021db80eec33a5f166310d53f1be265
Kieran O'Brien <kieran.obrien@ittybittyapps.com>
2021-04-28 17:17:32 +1000

    Add callbacks to widget

--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayWidgetView.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayWidgetView.kt
@@ -2,75 +2,120 @@ package com.afterpay.android.view
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Message
 import android.util.AttributeSet
+import android.util.Log
 import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import com.afterpay.android.Afterpay
 import com.afterpay.android.internal.Configuration
 import com.afterpay.android.model.Money
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combineTransform
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.take
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.math.BigDecimal
 
 class AfterpayWidgetView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : WebView(context, attrs, defStyleAttr),
-    CoroutineScope by CoroutineScope(Dispatchers.Main.immediate) {
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : WebView(context, attrs, defStyleAttr) {
 
     private val configuration: Configuration
         get() = checkNotNull(Afterpay.configuration) { "Afterpay configuration is not set" }
 
-    private val pageLoaded = MutableStateFlow(false)
-    private val token = MutableStateFlow<String?>(null)
-    private val totalCost = MutableStateFlow<BigDecimal?>(null)
-
     private val json = Json { ignoreUnknownKeys = true }
 
-    init {
-        combineTransform(pageLoaded.filter { it }, token, totalCost) { _, token, totalCost ->
-            token ?: totalCost ?: return@combineTransform
-            emit(token?.let { """"$it"""" } to totalCost?.toAmount())
+    fun init(
+        token: String,
+        onExternalRequest: (Uri) -> Unit,
+        onError: (String?) -> Unit
+    ) {
+        check(token.isNotBlank()) { "Supplied token is empty" }
+        configureWebView(onExternalRequest, onError) {
+            loadWidget(""""$token"""", totalCost = null)
         }
-            .take(1)
-            .onEach { (token, totalCost) -> init(token, totalCost) }
-            .launchIn(this)
+    }
 
+    fun init(
+        totalCost: BigDecimal,
+        onExternalRequest: (Uri) -> Unit,
+        onError: (String?) -> Unit
+    ) {
+        configureWebView(onExternalRequest, onError) {
+            loadWidget(token = null, totalCost.toAmount())
+        }
+    }
+
+    private fun configureWebView(
+        onExternalRequest: (Uri) -> Unit,
+        onError: (String?) -> Unit,
+        onPageFinished: () -> Unit
+    ) {
         @SuppressLint("SetJavaScriptEnabled")
         settings.javaScriptEnabled = true
         settings.domStorageEnabled = true
+        settings.setSupportMultipleWindows(true)
         addJavascriptInterface(this, "Android")
+
+        webChromeClient = object : WebChromeClient() {
+
+            override fun onCreateWindow(
+                webView: WebView?,
+                isDialog: Boolean,
+                isUserGesture: Boolean,
+                resultMsg: Message?
+            ): Boolean {
+                val message = webView?.handler?.obtainMessage()
+                webView?.requestFocusNodeHref(message)
+                message?.data?.getString("url")?.let { onExternalRequest(Uri.parse(it)) }
+                return false
+            }
+        }
 
         webViewClient = object : WebViewClient() {
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                pageLoaded.tryEmit(true)
+                onPageFinished()
+            }
+
+            @RequiresApi(Build.VERSION_CODES.M)
+            override fun onReceivedError(
+                webView: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                checkNotNull(webView) { "A WebView was expected but not received" }
+                if (request?.isForMainFrame == true) {
+                    onError(error?.description.toString())
+                }
+            }
+
+            override fun onReceivedError(
+                webView: WebView?,
+                errorCode: Int,
+                description: String?,
+                failingUrl: String?
+            ) {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                    checkNotNull(webView) { "A WebView was expected but not received" }
+                    onError(description)
+                }
             }
         }
 
         loadUrl("https://afterpay.github.io/sdk-example-server/widget-bootstrap.html")
     }
 
-    fun init(token: String) {
-        check(token.isNotBlank()) { "Supplied token is empty" }
-        this.token.tryEmit(token)
-    }
-
-    fun init(totalCost: BigDecimal) {
-        this.totalCost.tryEmit(totalCost)
-    }
-
-    private fun init(token: String?, totalCost: String?) {
+    private fun loadWidget(token: String?, totalCost: String?) {
         val script =
             """createAfterpayWidget($token, $totalCost, "${configuration.locale}", { "logo": true, "heading": true });"""
         evaluateJavascript(script, null)
@@ -86,16 +131,13 @@ class AfterpayWidgetView @JvmOverloads constructor(
 
     @JavascriptInterface
     fun postMessage(messageJson: String) {
-        kotlin.runCatching { json.decodeFromString<Event>(messageJson) }
+        runCatching { json.decodeFromString<Event>(messageJson) }
             .onSuccess { TODO() }
-            .onFailure {
-                it.printStackTrace()
-                // TODO()
-            }
+            .onFailure { TODO() }
     }
 
     @Serializable
-    internal data class Event(
+    private data class Event(
         val isValid: Boolean,
         val amountDueToday: Money? = null,
         val paymentScheduleChecksum: String? = null,
@@ -103,7 +145,7 @@ class AfterpayWidgetView @JvmOverloads constructor(
     ) {
 
         @Serializable
-        data class Error(
+        private data class Error(
             val errorCode: String? = null,
             val errorId: String? = null,
             val message: String? = null,
