e59cf65c3b010c29f2a253b397ee708002ad4d18
Kieran O'Brien <kieran.obrien@ittybittyapps.com>
2021-05-04 14:55:03 +1000

    Replace Moshi with Kotlin serialization

--- afterpay/src/main/kotlin/com/afterpay/android/model/Money.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/model/Money.kt
@@ -1,9 +1,13 @@
 package com.afterpay.android.model
 
+import com.afterpay.android.internal.BigDecimalSerializer
+import com.afterpay.android.internal.CurrencySerializer
 import kotlinx.serialization.Serializable
+import java.math.BigDecimal
+import java.util.Currency
 
 @Serializable
 data class Money(
-    val amount: String,
-    val currency: String
+    @Serializable(with = BigDecimalSerializer::class) val amount: BigDecimal,
+    @Serializable(with = CurrencySerializer::class) val currency: Currency
 )
