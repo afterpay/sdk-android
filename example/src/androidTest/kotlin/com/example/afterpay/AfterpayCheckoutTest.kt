f0af5ec03354636bbbe82314b181cd0eff79ac01
Kieran O'Brien <kieran.obrien@ittybittyapps.com>
2021-05-21 09:30:38 +1000

    Move test to correct package and update for widget

--- example/src/androidTest/kotlin/com/example/afterpay/AfterpayCheckoutTest.kt
+++ example/src/androidTest/kotlin/com/example/afterpay/AfterpayCheckoutTest.kt
@@ -1,6 +1,5 @@
-package com.afterpay.android
+package com.example.afterpay
 
-import android.util.Log
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.navigation.NavController
@@ -17,12 +16,18 @@ import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isNotChecked
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.web.assertion.WebViewAssertions.webContent
+import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
+import androidx.test.espresso.web.matcher.DomMatchers.hasElementWithId
+import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
+import androidx.test.espresso.web.webdriver.DriverAtoms.getText
+import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.afterpay.MainActivity
-import com.example.afterpay.R
-import com.example.afterpay.nav_graph
+import com.afterpay.android.DisableAnimationsRule
 import com.example.afterpay.shopping.ShoppingListAdapter
+import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
@@ -30,6 +35,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.atomic.AtomicBoolean
+
+private const val WIDGET_CONTAINER_ID = "afterpay-widget-container"
 
 @RunWith(AndroidJUnit4::class)
 class AfterpayCheckoutTest {
@@ -86,9 +93,26 @@ class AfterpayCheckoutTest {
 
         IdlingRegistry.getInstance().register(idlingResource)
 
-        onView(withId(R.id.receipt_totalCost_label)).check(matches(isDisplayed()))
+        onView(withId(R.id.receipt_afterpayWidget_container)).check(matches(isDisplayed()))
 
         IdlingRegistry.getInstance().unregister(idlingResource)
+
+        onWebView(withId(R.id.receipt_afterpayWidget))
+            .check(webContent(hasElementWithId(WIDGET_CONTAINER_ID)))
+
+        val widgetContainer = onWebView(withId(R.id.receipt_afterpayWidget))
+            .withElement(findElement(Locator.ID, WIDGET_CONTAINER_ID))
+
+        val expectedInitialContent =
+            """{"token":"thisIsAnOrderToken","amount":null,"locale":"en_AU","style":{"logo":true,"heading":true}}"""
+        widgetContainer.check(webMatches(getText(), containsString(expectedInitialContent)))
+
+        onView(withId(R.id.receipt_totalCost)).perform(typeText("50"))
+
+        Thread.sleep(800)
+
+        val expectedUpdatedContent = """Update called with {"amount":"50","currency":"AUD"}"""
+        widgetContainer.check(webMatches(getText(), containsString(expectedUpdatedContent)))
     }
 
     private fun clickChildWithId(@IdRes viewId: Int) = object : ViewAction {
