ea134d7f28dcf7a3b35fc10a7864723f0bc82af9
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2021-07-21 11:20:32 +1000

    Change var to val in CheckoutV3Consumer interface

--- afterpay/src/main/kotlin/com/afterpay/android/model/CheckoutV3Consumer.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/model/CheckoutV3Consumer.kt
@@ -2,15 +2,15 @@ package com.afterpay.android.model
 
 interface CheckoutV3Consumer {
     /** The consumer’s email address. Limited to 128 characters. **/
-    var email: String
+    val email: String
     /** The consumer’s first name and any middle names. Limited to 128 characters. **/
-    var givenNames: String?
+    val givenNames: String?
     /** The consumer’s last name. Limited to 128 characters. **/
-    var surname: String?
+    val surname: String?
     /** The consumer’s phone number. Limited to 32 characters. **/
-    var phoneNumber: String?
+    val phoneNumber: String?
     /** The consumer's shipping information. **/
-    var shippingInformation: CheckoutV3Contact?
+    val shippingInformation: CheckoutV3Contact?
     /** The consumer's billing information. **/
-    var billingInformation: CheckoutV3Contact?
+    val billingInformation: CheckoutV3Contact?
 }
