8b80edc5fb51d4eb6ba1183bd45f7ceab729ebc7
Scott Antonac <scott.antonac@afterpay.com>
2021-12-17 09:13:54 +1100

    add eu and fr-CA tests as well as EUR currency tests

--- afterpay/src/test/kotlin/com/afterpay/android/AfterpayInstalmentTest.kt
+++ afterpay/src/test/kotlin/com/afterpay/android/AfterpayInstalmentTest.kt
@@ -15,93 +15,117 @@ class AfterpayInstalmentTest {
     private val poundSterling: Currency = Currency.getInstance("GBP")
     private val newZealandDollar: Currency = Currency.getInstance("NZD")
     private val unitedStatesDollar: Currency = Currency.getInstance("USD")
+    private val euro: Currency = Currency.getInstance("EUR")
 
     private val oneHundredAndTwenty = 120.toBigDecimal()
     private val oneHundredAndTwentyOne = 121.toBigDecimal()
 
     @Test
-    fun `available instalment in Australia locale`() {
-        val locale = Locales.EN_AU
+    fun `available instalment in en-AU locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.EN_AU)
 
-        val audInstalment = availableInstalment(oneHundredAndTwenty, australianDollar, locale)
-        val cadInstalment = availableInstalment(oneHundredAndTwenty, canadianDollar, locale)
-        val gbpInstalment = availableInstalment(oneHundredAndTwenty, poundSterling, locale)
-        val nzdInstalment = availableInstalment(oneHundredAndTwenty, newZealandDollar, locale)
-        val usdInstalment = availableInstalment(oneHundredAndTwenty, unitedStatesDollar, locale)
-
-        assertEquals("$30.00", audInstalment.instalmentAmount)
-        assertEquals("$30.00 CAD", cadInstalment.instalmentAmount)
-        assertEquals("£30.00", gbpInstalment.instalmentAmount)
-        assertEquals("$30.00 NZD", nzdInstalment.instalmentAmount)
-        assertEquals("$30.00 USD", usdInstalment.instalmentAmount)
+        assertEquals("$30.00", instalments.aud.instalmentAmount)
+        assertEquals("$30.00 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30.00", instalments.gbp.instalmentAmount)
+        assertEquals("$30.00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30.00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30.00€", instalments.eur.instalmentAmount)
     }
 
     @Test
-    fun `available instalment in Canada locale`() {
-        val locale = Locales.EN_CA
+    fun `available instalment in en-CA locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.EN_CA)
 
-        val audInstalment = availableInstalment(oneHundredAndTwenty, australianDollar, locale)
-        val cadInstalment = availableInstalment(oneHundredAndTwenty, canadianDollar, locale)
-        val gbpInstalment = availableInstalment(oneHundredAndTwenty, poundSterling, locale)
-        val nzdInstalment = availableInstalment(oneHundredAndTwenty, newZealandDollar, locale)
-        val usdInstalment = availableInstalment(oneHundredAndTwenty, unitedStatesDollar, locale)
-
-        assertEquals("$30.00 AUD", audInstalment.instalmentAmount)
-        assertEquals("$30.00", cadInstalment.instalmentAmount)
-        assertEquals("£30.00", gbpInstalment.instalmentAmount)
-        assertEquals("$30.00 NZD", nzdInstalment.instalmentAmount)
-        assertEquals("$30.00 USD", usdInstalment.instalmentAmount)
+        assertEquals("$30.00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("$30.00", instalments.cad.instalmentAmount)
+        assertEquals("£30.00", instalments.gbp.instalmentAmount)
+        assertEquals("$30.00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30.00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30.00€", instalments.eur.instalmentAmount)
     }
 
     @Test
-    fun `available instalment in UK locale`() {
-        val locale = Locales.EN_GB
+    fun `available instalment in fr-CA locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.FR_CA)
 
-        val audInstalment = availableInstalment(oneHundredAndTwenty, australianDollar, locale)
-        val cadInstalment = availableInstalment(oneHundredAndTwenty, canadianDollar, locale)
-        val gbpInstalment = availableInstalment(oneHundredAndTwenty, poundSterling, locale)
-        val nzdInstalment = availableInstalment(oneHundredAndTwenty, newZealandDollar, locale)
-        val usdInstalment = availableInstalment(oneHundredAndTwenty, unitedStatesDollar, locale)
-
-        assertEquals("$30.00 AUD", audInstalment.instalmentAmount)
-        assertEquals("$30.00 CAD", cadInstalment.instalmentAmount)
-        assertEquals("£30.00", gbpInstalment.instalmentAmount)
-        assertEquals("$30.00 NZD", nzdInstalment.instalmentAmount)
-        assertEquals("$30.00 USD", usdInstalment.instalmentAmount)
+        assertEquals("$30,00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("30,00 $", instalments.cad.instalmentAmount)
+        assertEquals("£30,00", instalments.gbp.instalmentAmount)
+        assertEquals("$30,00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30,00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30,00€", instalments.eur.instalmentAmount)
     }
 
     @Test
-    fun `available instalment in New Zealand locale`() {
-        val locale = Locales.EN_NZ
+    fun `available instalment in en-GB locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.EN_GB)
 
-        val audInstalment = availableInstalment(oneHundredAndTwenty, australianDollar, locale)
-        val cadInstalment = availableInstalment(oneHundredAndTwenty, canadianDollar, locale)
-        val gbpInstalment = availableInstalment(oneHundredAndTwenty, poundSterling, locale)
-        val nzdInstalment = availableInstalment(oneHundredAndTwenty, newZealandDollar, locale)
-        val usdInstalment = availableInstalment(oneHundredAndTwenty, unitedStatesDollar, locale)
-
-        assertEquals("$30.00 AUD", audInstalment.instalmentAmount)
-        assertEquals("$30.00 CAD", cadInstalment.instalmentAmount)
-        assertEquals("£30.00", gbpInstalment.instalmentAmount)
-        assertEquals("$30.00", nzdInstalment.instalmentAmount)
-        assertEquals("$30.00 USD", usdInstalment.instalmentAmount)
+        assertEquals("$30.00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("$30.00 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30.00", instalments.gbp.instalmentAmount)
+        assertEquals("$30.00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30.00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30.00€", instalments.eur.instalmentAmount)
     }
 
     @Test
-    fun `available instalment in US locale`() {
-        val locale = Locales.EN_US
+    fun `available instalment in en-NZ locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.EN_NZ)
 
-        val audInstalment = availableInstalment(oneHundredAndTwenty, australianDollar, locale)
-        val cadInstalment = availableInstalment(oneHundredAndTwenty, canadianDollar, locale)
-        val gbpInstalment = availableInstalment(oneHundredAndTwenty, poundSterling, locale)
-        val nzdInstalment = availableInstalment(oneHundredAndTwenty, newZealandDollar, locale)
-        val usdInstalment = availableInstalment(oneHundredAndTwenty, unitedStatesDollar, locale)
+        assertEquals("$30.00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("$30.00 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30.00", instalments.gbp.instalmentAmount)
+        assertEquals("$30.00", instalments.nzd.instalmentAmount)
+        assertEquals("$30.00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30.00€", instalments.eur.instalmentAmount)
+    }
 
-        assertEquals("A$30.00", audInstalment.instalmentAmount)
-        assertEquals("CA$30.00", cadInstalment.instalmentAmount)
-        assertEquals("£30.00", gbpInstalment.instalmentAmount)
-        assertEquals("NZ$30.00", nzdInstalment.instalmentAmount)
-        assertEquals("$30.00", usdInstalment.instalmentAmount)
+    @Test
+    fun `available instalment in en-US locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.EN_US)
+
+        assertEquals("A$30.00", instalments.aud.instalmentAmount)
+        assertEquals("CA$30.00", instalments.cad.instalmentAmount)
+        assertEquals("£30.00", instalments.gbp.instalmentAmount)
+        assertEquals("NZ$30.00", instalments.nzd.instalmentAmount)
+        assertEquals("$30.00", instalments.usd.instalmentAmount)
+        assertEquals("30.00€", instalments.eur.instalmentAmount)
+    }
+
+    @Test
+    fun `available instalment in it-IT locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.IT_IT)
+
+        assertEquals("$30,00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("$30,00 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30,00", instalments.gbp.instalmentAmount)
+        assertEquals("$30,00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30,00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30,00 €", instalments.eur.instalmentAmount)
+    }
+
+    @Test
+    fun `available instalment in fr-FR locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.FR_FR)
+
+        assertEquals("$30,00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("$30,00 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30,00", instalments.gbp.instalmentAmount)
+        assertEquals("$30,00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30,00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30,00 €", instalments.eur.instalmentAmount)
+    }
+
+    @Test
+    fun `available instalment in es-ES locale`() {
+        val instalments = createAllInstalments(oneHundredAndTwenty, Locales.ES_ES)
+
+        assertEquals("$30,00 AUD", instalments.aud.instalmentAmount)
+        assertEquals("$30,00 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30,00", instalments.gbp.instalmentAmount)
+        assertEquals("$30,00 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30,00 USD", instalments.usd.instalmentAmount)
+        assertEquals("30,00 €", instalments.eur.instalmentAmount)
     }
 
     /**
@@ -114,19 +138,34 @@ class AfterpayInstalmentTest {
      */
     @Test
     fun `available instalment when amount is round and odd`() {
-        val locale = Locales.EN_AU
+        val instalments = createAllInstalments(oneHundredAndTwentyOne, Locales.EN_AU)
 
-        val audInstalment = availableInstalment(oneHundredAndTwentyOne, australianDollar, locale)
-        val cadInstalment = availableInstalment(oneHundredAndTwentyOne, canadianDollar, locale)
-        val gbpInstalment = availableInstalment(oneHundredAndTwentyOne, poundSterling, locale)
-        val nzdInstalment = availableInstalment(oneHundredAndTwentyOne, newZealandDollar, locale)
-        val usdInstalment = availableInstalment(oneHundredAndTwentyOne, unitedStatesDollar, locale)
+        assertEquals("$30.25", instalments.aud.instalmentAmount)
+        assertEquals("$30.25 CAD", instalments.cad.instalmentAmount)
+        assertEquals("£30.25", instalments.gbp.instalmentAmount)
+        assertEquals("$30.25 NZD", instalments.nzd.instalmentAmount)
+        assertEquals("$30.25 USD", instalments.usd.instalmentAmount)
+        assertEquals("30.25€", instalments.eur.instalmentAmount)
+    }
 
-        assertEquals("$30.25", audInstalment.instalmentAmount)
-        assertEquals("$30.25 CAD", cadInstalment.instalmentAmount)
-        assertEquals("£30.25", gbpInstalment.instalmentAmount)
-        assertEquals("$30.25 NZD", nzdInstalment.instalmentAmount)
-        assertEquals("$30.25 USD", usdInstalment.instalmentAmount)
+    private data class AllInstallments(
+        val aud: AfterpayInstalment.Available,
+        val cad: AfterpayInstalment.Available,
+        val gbp: AfterpayInstalment.Available,
+        val nzd: AfterpayInstalment.Available,
+        val usd: AfterpayInstalment.Available,
+        val eur: AfterpayInstalment.Available
+    )
+
+    private fun createAllInstalments(amount: BigDecimal, locale: Locale): AllInstallments {
+        return AllInstallments(
+            aud = availableInstalment(amount, australianDollar, locale),
+            cad = availableInstalment(amount, canadianDollar, locale),
+            gbp = availableInstalment(amount, poundSterling, locale),
+            nzd = availableInstalment(amount, newZealandDollar, locale),
+            usd = availableInstalment(amount, unitedStatesDollar, locale),
+            eur = availableInstalment(amount, euro, locale)
+        )
     }
 
     private fun availableInstalment(
