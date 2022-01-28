3a5f20046bda446d1bdb315a13992ccdec31c03c
Scott Antonac <scott.antonac@afterpay.com>
2022-01-28 14:00:25 +1100

    fix linting issues in AfterpayMoreInfoOptions

--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayMoreInfoOptions.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayMoreInfoOptions.kt
@@ -7,13 +7,13 @@ class AfterpayMoreInfoOptions(
     var modalId: String? = null,
     var isCBT: Boolean = false
 ) {
-    internal fun modalFile() : String {
+    internal fun modalFile(): String {
         modalId?.let {
             return "$it.html"
         }
 
         val locale = "${Afterpay.locale.language}_${Afterpay.locale.country}"
-        val cbt = if(isCBT) "-cbt" else ""
+        val cbt = if (isCBT) "-cbt" else ""
         val theme = modalTheme.slug
 
         return "$locale$theme$cbt.html"
