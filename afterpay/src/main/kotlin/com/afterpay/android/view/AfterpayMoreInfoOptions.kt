8c34ebd0e4eedfdfda590e485caa830eb5b0bd3c
Scott Antonac <scott.antonac@afterpay.com>
2022-01-31 11:29:44 +1100

    document AfterpayMoreInfoOptions and reorder params

--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayMoreInfoOptions.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayMoreInfoOptions.kt
@@ -2,9 +2,20 @@ package com.afterpay.android.view
 
 import com.afterpay.android.Afterpay
 
+/**
+ * Setting up options for the more info link in AfterpayPriceBreakdown
+ *
+ * **Notes:**
+ * - If both `modalId` is set, `modalTheme` and `isCbtEnabled` are ignored.
+ * - Not all combinations of Locales and CBT are available.
+ *
+ * @param modalId the filename of a modal hosted on Afterpay static
+ * @param modalTheme the color theme used when displaying the modal
+ * @param isCbtEnabled whether to show the Cross Border Trade details in the modal
+ */
 class AfterpayMoreInfoOptions(
-    var modalTheme: AfterpayModalTheme = AfterpayModalTheme.MINT,
     var modalId: String? = null,
+    var modalTheme: AfterpayModalTheme = AfterpayModalTheme.MINT,
     var isCbtEnabled: Boolean = false
 ) {
     internal fun modalFile(): String {
