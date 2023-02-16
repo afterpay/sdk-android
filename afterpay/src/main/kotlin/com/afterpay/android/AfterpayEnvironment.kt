87e61be38b6d68c62e6ec827f5ff3eacc38394e4
Scott Antonac <santonac@squareup.com>
2023-02-17 10:51:24 +1100

    EIT-2304: fix environment api const

--- afterpay/src/main/kotlin/com/afterpay/android/AfterpayEnvironment.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/AfterpayEnvironment.kt
@@ -4,7 +4,7 @@ import java.net.URL
 import java.util.Locale
 
 const val API_PLUS_SANDBOX_BASE_URL = "https://api-plus.us-sandbox.afterpay.com"
-const val API_PLUS_PRODUCTION_BASE_URL = "https://api-plus.us-sandbox.afterpay.com"
+const val API_PLUS_PRODUCTION_BASE_URL = "https://api-plus.us.afterpay.com"
 
 enum class AfterpayEnvironment(
     val payKitId: String,
