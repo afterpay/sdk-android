c1aeffcdeba5494feceb7999dda583d9ffa68cdd
Scott Antonac <santonac@squareup.com>
2023-02-17 10:59:31 +1100

    EIT-2304: rename classes in example app

--- example/src/main/kotlin/com/example/afterpay/checkout/CashAppHandler.kt
+++ example/src/main/kotlin/com/example/afterpay/checkout/CashAppHandler.kt
@@ -1,12 +1,12 @@
 package com.example.afterpay.checkout
 
 import com.afterpay.android.cashapp.AfterpayCashAppHandler
-import com.afterpay.android.cashapp.CashAppCreateOrderResult
+import com.afterpay.android.cashapp.CashAppSignOrderResult
 
 class CashAppHandler(
-    val onDidReceiveResponse: (CashAppCreateOrderResult) -> Unit,
+    val onDidReceiveResponse: (CashAppSignOrderResult) -> Unit,
 ) : AfterpayCashAppHandler {
-    override fun didReceiveCashAppData(cashAppData: CashAppCreateOrderResult) {
+    override fun didReceiveCashAppData(cashAppData: CashAppSignOrderResult) {
         onDidReceiveResponse(cashAppData)
     }
 }
