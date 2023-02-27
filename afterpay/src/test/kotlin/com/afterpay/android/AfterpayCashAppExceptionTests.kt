294878df8e89cd6592584862d158e950268d51d3
Scott Antonac <santonac@squareup.com>
2023-02-27 12:12:57 +1100

    EIT-2304: add exception tests for cash

---
+++ afterpay/src/test/kotlin/com/afterpay/android/AfterpayCashAppExceptionTests.kt
@@ -0,0 +1,22 @@
+package com.afterpay.android
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class AfterpayCashAppExceptionTests {
+    @Test
+    fun `should throw on signCashAppOrder if handler not setup`() {
+        val exception = assertThrows(Exception::class.java) {
+            runTest {
+                Afterpay.signCashAppOrder("123")
+            }
+        }
+
+        assertEquals("cashAppHandler or the handler parameter must be set and not null before attempting to sign a Cash App order", exception.message)
+    }
+}
+
