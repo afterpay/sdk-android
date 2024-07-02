317ad781b3c299a7031f3ea663e344d0666ca685
Jason Atwood <jatwood@squareup.com>
2024-07-02 18:04:39 -0400

    Add spotless/ktlint gradle plugin

--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayCheckoutV2Activity.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayCheckoutV2Activity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Afterpay
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.afterpay.android.view
 
 import android.annotation.SuppressLint
@@ -40,7 +55,6 @@ import com.afterpay.android.internal.getCheckoutV2OptionsExtra
 import com.afterpay.android.internal.putCancellationStatusExtra
 import com.afterpay.android.internal.putOrderTokenExtra
 import com.afterpay.android.internal.setAfterpayUserAgentString
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.util.Locale
@@ -70,7 +84,7 @@ internal class AfterpayCheckoutV2Activity : AppCompatActivity() {
         window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
 
         loadingWebView = findViewById<WebView>(R.id.afterpay_loadingWebView).apply {
-            val htmlData = Base64.encodeToString(Html.loading.toByteArray(), Base64.NO_PADDING)
+            val htmlData = Base64.encodeToString(Html.LOADING.toByteArray(), Base64.NO_PADDING)
             loadData(htmlData, "text/html", "base64")
         }
 
--- afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayCheckoutV3Activity.kt
+++ afterpay/src/main/kotlin/com/afterpay/android/view/AfterpayCheckoutV3Activity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Afterpay
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.afterpay.android.view
 
 import android.annotation.SuppressLint
@@ -51,7 +66,7 @@ internal class AfterpayCheckoutV3Activity : AppCompatActivity() {
                 received = ::received,
             )
             webChromeClient = AfterpayWebChromeClientV3(openExternalLink = ::open)
-            val htmlData = Base64.encodeToString(Html.loading.toByteArray(), Base64.NO_PADDING)
+            val htmlData = Base64.encodeToString(Html.LOADING.toByteArray(), Base64.NO_PADDING)
             loadData(htmlData, "text/html", "base64")
         }
 
